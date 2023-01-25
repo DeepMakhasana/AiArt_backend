35341b02f2fbae577dfbdac0cd723efb35c1ad13
DeepMakhasana <deepmakhasana1013@gmail.com>
2023-01-25 10:07:26 +0530

    upload Backend

--- router/admin.js
+++ router/admin.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const cookieParser = require("cookie-parser");
 const {upload, imageUpload} = require("../controller/ImageUpload");
 
-router.get("/", cookieParser('secret'), (req,res) => {
-    res.json(req.signedCookies);
-})
 router.post("/upload", upload, imageUpload);
 
 module.exports = router;
--- router/normal.js
+++ router/normal.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const cookieParser = require("cookie-parser");
 
 const Register = require("../controller/Register");
 const Login = require("../controller/Login");
@@ -8,7 +7,7 @@ const Images = require("../controller/Images");
 const ImageByName = require("../controller/ImageByName");
 
 
-router.post("/register", cookieParser('secret'), Register);
+router.post("/register", Register);
 router.post("/login", Login);
 router.get("/images", Images);
 router.get("/image/:name", ImageByName);
